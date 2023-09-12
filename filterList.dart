c3a6215fad85166a36ec498b51df7bd2ee76e757
Shadow <edwardchirwa07@gmail.com>
2023-09-12 12:47:31 +0200

    first commit

---
+++ filterList.dart
@@ -0,0 +1,61 @@
+import 'notes.dart';
+import 'dart:io';
+
+var run = new UserInteraction();
+void main() {
+  //init method
+  //listen to user input
+
+  stdin.lineMode = true;
+  stdin.echoMode = true;
+  String search = "";
+
+  run.promptUser(search);
+  // print("Type something and press enter: ");
+
+  //start listen for input
+}
+
+class UserInteraction {
+  List<Note> filteredList = sample;
+  int count = 0;
+  void promptUser(String search) {
+    // print("Search :");
+    stdout.write("Search : ");
+    stdin.listen((data) {
+      String userInput = String.fromCharCodes(data).trim();
+
+      // for (var notes in sample) {
+      //   print(notes.content);
+      // }
+
+      if (userInput.isNotEmpty) {
+        search += userInput;
+        getSearchText(search);
+        for (var notes in filteredList) {
+          print("Title  :" + notes.title);
+          print("Body   :" + notes.content);
+          print("");
+          this.count++;
+        }
+        if (this.count > 1) {
+          print("${count} notes were found!");
+        } else {
+          print("${count} note was found!");
+        }
+
+        this.count = 0;
+        search = "";
+        filteredList = sample;
+        stdout.write("Search : ");
+      }
+    });
+  }
+
+  void getSearchText(String searchText) {
+    filteredList = sample
+        .where((note) =>
+            note.title.toLowerCase().contains(searchText.toLowerCase()))
+        .toList();
+  }
+}
---
+++ notes.dart
@@ -0,0 +1,12 @@
+class Note {
+  int id;
+  String title;
+  String content;
+
+  Note({required this.id, required this.title, required this.content});
+}
+
+List<Note> sample = [
+  Note(id: 0, title: "cod3ddy", content: "-make a shadow web"),
+  Note(id: 0, title: "Cookies", content: "-sell cookies"),
+];
